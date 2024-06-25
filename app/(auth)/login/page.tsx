0e55deecbd4c522411ab8597c9ce65e4280af2b2
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-06-25 14:49:52 +0200

    refractor: refractor login form into individual component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,4 @@
-import { credentialsLogin } from '@/src/actions/actions';
-import FormInput from '@/src/components/global/formInput/FormInput';
-import FormPasswordInput from '@/src/components/global/formPasswordInput/FormPasswordInput';
-import ForgotPasswordLink from '@/src/components/login/forgotPasswordLink/ForgotPasswordLink';
-import LoginButton from '@/src/components/login/loginButton/LoginButton';
+import LoginForm from '@/src/components/login/loginForm/LoginForm';
 import LoginFormHeading from '@/src/components/login/loginFormHeading/LoginFormHeading';
 
 function Login() {
@@ -12,17 +8,7 @@ function Login() {
         heading="Welcome Back!"
         subheading="Please, provide your admin credentials to log in."
       />
-      <form action={credentialsLogin} className="flex flex-col">
-        <FormInput size="sm" label="Email" name="email" />
-        <FormPasswordInput>
-          <FormInput size="sm" label="Password" name="password" />
-        </FormPasswordInput>
-
-        <div className="flex justify-end pb-12">
-          <ForgotPasswordLink />
-        </div>
-        <LoginButton />
-      </form>
+      <LoginForm />
     </>
   );
 }
---
+++ src/components/login/loginForm/LoginForm.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { credentialsLogin } from '@/src/actions/actions';
+import { TSignInSchema, signInSchema } from '@/src/utils/types/zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import FormInput from '../../global/formInput/FormInput';
+import FormPasswordInput from '../../global/formPasswordInput/FormPasswordInput';
+import PrimaryButton from '../../global/primaryButton/PrimaryButton';
+import ForgotPasswordLink from '../forgotPasswordLink/ForgotPasswordLink';
+
+function LoginForm() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<TSignInSchema>({ resolver: zodResolver(signInSchema) });
+
+  async function onSubmit(data: TSignInSchema) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const res = await credentialsLogin(data.email, data.password);
+
+    if (res.status === 'success') {
+      router.push('/dashboard');
+    }
+    console.log(res.message);
+
+    reset();
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      <FormInput
+        size="sm"
+        label="Email"
+        connectFunction={register}
+        id="email"
+        error={errors.email?.message as string}
+      />
+      <FormPasswordInput>
+        <FormInput
+          size="sm"
+          label="Password"
+          connectFunction={register}
+          id="password"
+          error={errors.password?.message as string}
+        />
+      </FormPasswordInput>
+
+      <div className="flex justify-end pb-12">
+        <ForgotPasswordLink />
+      </div>
+      <PrimaryButton
+        type="submit"
+        isLoading={isSubmitting}
+        content="Login"
+        additionalClass=""
+      />
+    </form>
+  );
+}
+
+export default LoginForm;
